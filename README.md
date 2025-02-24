64632dbe63e703f4f1c4036f86dbf470f3079cab
Luan Himmlisch <luan@himmlisch.com.mx>
2025-02-23 19:37:36 -0600

    Single HTML export

--- README.md
+++ README.md
@@ -3,15 +3,23 @@
 This is a small demo of the DDA Graph using [KaplayJS](https://kaplayjs.com/) as the rendering engine.
 
 > [!IMPORTANT]  
-> The files *public/music/background.wav* and *public/sounds/explosions-[1-4]* were removed due to licensing.
+> The files *src/music/background.wav* and *src/sounds/explosions-[1-4]* were removed due to licensing.
 > Import your own music and sounds to run.
 
 ## How to run?
 
-With NodeJS simply run
+First, install the dependencies using NodeJS:
+
+```
+npm install
+```
+
+Then, simply run
 
 ```
 npm run dev
 ```
 
 Use left-click and right-click to setup the two points. Or use the form below to input the point coords manually.
+
+To build, you need to have installed `ffmpeg` in your system.
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from "vite";
 import svgLoader from 'vite-svg-loader';
+import { viteSingleFile } from "vite-plugin-singlefile";
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { imagetools } from 'vite-imagetools'
+import viteAudioTransform from "vite-audio-transform";
 
 export default defineConfig({
     base: "./",
+    build: {
+        assetsInlineLimit: 2.097152e+6, // 2mb
+        chunkSizeWarningLimit: 10000, // 10mb
+    },
     plugins: [
         imagetools({
             defaultDirectives: () => new URLSearchParams({
                 format: 'webp',
+                inline: true,
                 lossless: true
             })
         }),
         createHtmlPlugin({ minify: true }),
         svgLoader({ defaultImport: 'url' }),
+        viteAudioTransform({ type: 'webm' }),
+        viteSingleFile(),
     ],
 });
